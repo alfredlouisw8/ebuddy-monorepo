3bdb7ce4655b4b02ef549ae64faa954af6dba053
Alfred Louis <alfredlouisw8@gmail.com>
2025-03-25 21:55:46 +0700

    wip shared package

--- apps/backend/src/config/firebaseConfig.ts
+++ apps/backend/src/config/firebaseConfig.ts
@@ -2,12 +2,16 @@ import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 import dotenv from "dotenv";
+import {
+	FIRESTORE_EMULATOR_HOST,
+	FIREBASE_AUTH_EMULATOR_HOST,
+} from "@repo/shared/constants/firebase";
 
 dotenv.config();
 
 if (process.env.NODE_ENV !== "production") {
-	process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
-	process.env.FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099";
+	process.env.FIRESTORE_EMULATOR_HOST = FIRESTORE_EMULATOR_HOST;
+	process.env.FIREBASE_AUTH_EMULATOR_HOST = FIREBASE_AUTH_EMULATOR_HOST;
 }
 
 const app = initializeApp({
